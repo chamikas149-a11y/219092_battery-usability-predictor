a6c1f08bee9a878b3c9469700ad4ec8191b6b53a
chamikas149-a11y <chamikas149@gmail.com>
2026-04-06 10:44:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ CLASS_COLORS = {'Poor':'#ff3366','Fair':'#ff6b35','Good':'#00ff9d'}
 def load_models():
     try:
         import os
-        os.environ['KERAS_BACKEND'] = 'jax'
+        os.environ['KERAS_BACKEND'] = 'numpy'
         import keras
         reg    = keras.models.load_model('best_lstm_regression.keras')
         cls    = keras.models.load_model('best_lstm_classification.keras')
